01f1be989a425be3de5176ba3b2574f9a534e120
Otmar Ertl <otmar.ertl@gmail.com>
2017-03-13 20:50:35 +0100

    removed obsolete loop

--- python/zipf_mandelbrot_random_generator.py
+++ python/zipf_mandelbrot_random_generator.py
@@ -44,52 +44,51 @@ def ZipfMandelbrotRandomGenerator(n, q, v = 1, seed = None):
     rnd = Random(seed)
 
     while True:
-        while True:
-            u = Hn + rnd.random() * (Hx0 - Hn)
-            # u is uniformly distributed in (Hx0, Hn]
+        u = Hn + rnd.random() * (Hx0 - Hn)
+        # u is uniformly distributed in (Hx0, Hn]
 
-            x = H_inv(u)
-            k = int(x + 0.5)
+        x = H_inv(u)
+        k = int(x + 0.5)
 
-            # limit k to the range [0, n - 1], since
-            # k could be outside due to numerical inaccuracies
-            if k < 0:
-                k = 0
-            elif k > n - 1:
-                k = n - 1
+        # limit k to the range [0, n - 1], since
+        # k could be outside due to numerical inaccuracies
+        if k < 0:
+            k = 0
+        elif k > n - 1:
+            k = n - 1
 
-            # Here, the distribution of k is given by:
+        # Here, the distribution of k is given by:
+        #
+        #   P(k = 0) = C * (H(0.5) - Hx0) = C
+        #   P(k = m) = C * (H(m + 1/2) - H(m - 1/2)) for 1 <= m < n
+        #
+        #   where C := 1 / (Hn - Hx0)
+
+        if (k - x <= s or u >= H(k + 0.5) - h(k)):
+
+            # Case k = 0:
             #
-            #   P(k = 0) = C * (H(0.5) - Hx0) = C
-            #   P(k = m) = C * (H(m + 1/2) - H(m - 1/2)) for 1 <= m < n
+            #   The right inequality is always true, because replacing k by 0 gives
+            #   u >= H(0.5) - h(0) = Hx0 and u is taken from
+            #   (Hx0, Hn].
             #
-            #   where C := 1 / (Hn - Hx0)
+            #   Therefore, the acceptance rate for k = 1 is P(accepted | k = 1) = 1
+            #   and the probability that 1 is returned as random value is
+            #   P(k = 1 and accepted) = P(accepted | k = 1) * P(k = 1) = C = C / v^q
+            #
+            # Case k >= 1:
+            #
+            #   The left inequality (k - x <= s) is just a short cut
+            #   to avoid the more expensive evaluation of the right inequality
+            #   (u >= H(k + 0.5) - h(k)) in many cases.
+            #
+            #   Hence, the right inequality determines the acceptance rate:
+            #   H(m+1/2) - h(m) >= H(m-1/2), because h is convex
+            #   P(accepted | k = m) = h(m) / (H(m+1/2) - H(m-1/2))
+            #   The probability that m is returned is given by
+            #   P(k = m and accepted) = P(accepted | k = m) * P(k = m) = C * h(m) = C / (v + m)^q.
+            #
+            # In both cases the probabilities are proportional to the probability mass function
+            # of the Zipf distribution.
 
-            if (k - x <= s or u >= H(k + 0.5) - h(k)):
-
-                # Case k = 0:
-                #
-                #   The right inequality is always true, because replacing k by 0 gives
-                #   u >= H(0.5) - h(0) = Hx0 and u is taken from
-                #   (Hx0, Hn].
-                #
-                #   Therefore, the acceptance rate for k = 1 is P(accepted | k = 1) = 1
-                #   and the probability that 1 is returned as random value is
-                #   P(k = 1 and accepted) = P(accepted | k = 1) * P(k = 1) = C = C / v^q
-                #
-                # Case k >= 1:
-                #
-                #   The left inequality (k - x <= s) is just a short cut
-                #   to avoid the more expensive evaluation of the right inequality
-                #   (u >= H(k + 0.5) - h(k)) in many cases.
-                #
-                #   Hence, the right inequality determines the acceptance rate:
-                #   H(m+1/2) - h(m) >= H(m-1/2), because h is convex
-                #   P(accepted | k = m) = h(m) / (H(m+1/2) - H(m-1/2))
-                #   The probability that m is returned is given by
-                #   P(k = m and accepted) = P(accepted | k = m) * P(k = m) = C * h(m) = C / (v + m)^q.
-                #
-                # In both cases the probabilities are proportional to the probability mass function
-                # of the Zipf distribution.
-
-                yield k
+            yield k
