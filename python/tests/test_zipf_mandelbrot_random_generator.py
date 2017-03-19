ea359f5626d292e85686cd9a305a09b9781ede83
Otmar Ertl <otmar.ertl@gmail.com>
2017-03-19 20:51:33 +0100

    refactored and extedned unit tests

--- python/tests/test_zipf_mandelbrot_random_generator.py
+++ python/tests/test_zipf_mandelbrot_random_generator.py
@@ -9,11 +9,8 @@ import scipy.stats
 
 class TestZipfMandelbrotGenerator(unittest.TestCase):
 
-    def test1(self):
+    def run_test(self, n, q, v):
         N = 1000000
-        n = 10
-        q = 1
-        v = 1
 
         expectedRaw = numpy.array([pow(v+k,-q) for k in range(0, n)])
         expectedRawSum = expectedRaw.sum
@@ -24,56 +21,45 @@ class TestZipfMandelbrotGenerator(unittest.TestCase):
             observed[next(gen)] += 1
         _, p = scipy.stats.chisquare(observed, expected)
         self.assertGreater(p, 0.001)
+
+    def test1(self):
+        self.run_test(n = 10, q = 1, v = 1)
 
     def test2(self):
-        N = 1000000
-        n = 11
-        q = 0.5
-        v = 0.5
-
-        expectedRaw = numpy.array([pow(v+k,-q) for k in range(0, n)])
-        expectedRawSum = expectedRaw.sum
-        expected = expectedRaw/(numpy.sum(expectedRaw)) * N
-        gen = zipf_mandelbrot_random_generator.ZipfMandelbrotRandomGenerator(n, q, v)
-        observed = numpy.zeros(n)
-        for i in range(0,N):
-            observed[next(gen)] += 1
-        _, p = scipy.stats.chisquare(observed, expected)
-        self.assertGreater(p, 0.001)
+        self.run_test(n = 11, q = 0.5, v = 0.5)
 
     def test3(self):
-        N = 1000000
-        n = 7
-        q = 1.5
-        v = 1.5
-
-        expectedRaw = numpy.array([pow(v+k,-q) for k in range(0, n)])
-        expectedRawSum = expectedRaw.sum
-        expected = expectedRaw/(numpy.sum(expectedRaw)) * N
-        gen = zipf_mandelbrot_random_generator.ZipfMandelbrotRandomGenerator(n, q, v)
-        observed = numpy.zeros(n)
-        for i in range(0,N):
-            observed[next(gen)] += 1
-        _, p = scipy.stats.chisquare(observed, expected)
-        self.assertGreater(p, 0.001)
+        self.run_test(n = 7, q = 1.5, v = 1.5)
 
     def test4(self):
-        N = 1000000
-        n = 8
-        q = 0
-        v = 0.5
+        self.run_test(n = 8, q = 0, v = 0.5)
 
-        expectedRaw = numpy.array([pow(v+k,-q) for k in range(0, n)])
-        expectedRawSum = expectedRaw.sum
-        expected = expectedRaw/(numpy.sum(expectedRaw)) * N
-        gen = zipf_mandelbrot_random_generator.ZipfMandelbrotRandomGenerator(n, q, v)
-        observed = numpy.zeros(n)
-        for i in range(0,N):
-            observed[next(gen)] += 1
-        _, p = scipy.stats.chisquare(observed, expected)
-        self.assertGreater(p, 0.001)
+    def test5(self):
+        self.run_test(n = 8, q = numpy.nextafter(1,1), v = 0.5)
 
+    def test6(self):
+        self.run_test(n = 8, q = numpy.nextafter(1,-1), v = 0.5)
 
+    def test7(self):
+        self.run_test(n = 8, q = 0.5, v = 1000)
+
+    def test8(self):
+        self.run_test(n = 10, q = 1, v = 0.1)
+
+    def test9(self):
+        self.run_test(n = 10, q = 2, v = 0.1)
+
+    def test10(self):
+        self.run_test(n = 10, q = 0.5, v = 0.1)
+
+    def test11(self):
+        self.run_test(n = 10, q = 0.5, v = 0.01)
+
+    def test12(self):
+        self.run_test(n = 10, q = 0.1, v = 0.01)
+
+    def test13(self):
+        self.run_test(n = 10, q = 0.01, v = 0.01)
 
 if __name__ == '__main__':
     unittest.main()
